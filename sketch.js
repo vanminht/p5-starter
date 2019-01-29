1f8d4ff65cabdf879cd691c1cf61976afd7c7f33
vanminht <vanminht1994@gmail.com>
2019-01-29 15:51:22 -0800

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+let cnv;
+
+function setup() {
+    cnv = createCanvas(400, 400);
+
+    cnv.style('display', 'block'); // fix scrollbar appeared
+    cnv.parent('cnv');
+}
+
+function draw() {
+    background(51);
+
+}
